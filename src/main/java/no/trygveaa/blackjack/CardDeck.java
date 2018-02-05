74d2cd70296f0b5ed9aa8113156e66a54eda0cfb
Trygve Aaberge <trygveaa@gmail.com>
2018-02-06 00:11:35 +0100

    feat: Add method to draw from CardDeck
    This will remove and return the first card in the list of cards. If the
    deck is empty it will return an empty Optional.
    
    The constructors are changed to create a new ArrayList so this method
    won't fail if the constructor were given a list which doesn't support
    removing.

--- src/main/java/no/trygveaa/blackjack/CardDeck.java
+++ src/main/java/no/trygveaa/blackjack/CardDeck.java
@@ -1,8 +1,10 @@
 package no.trygveaa.blackjack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CardDeck {
@@ -13,15 +15,22 @@ public class CardDeck {
                 .flatMap(suit -> (
                         Arrays.stream(CardRank.values()).map(rank -> new Card(suit, rank))
                 ))
-                .collect(Collectors.toList());
+                .collect(Collectors.toCollection(ArrayList::new));
         Collections.shuffle(this.cards);
     }
 
     public CardDeck(List<Card> cards) {
-        this.cards = cards;
+        this.cards = new ArrayList<>(cards);
     }
 
     public List<Card> getCards() {
         return this.cards;
+    }
+
+    public Optional<Card> draw() {
+        if (this.cards.size() == 0) {
+            return Optional.empty();
+        }
+        return Optional.of(this.cards.remove(0));
     }
 }
--- src/test/java/no/trygveaa/blackjack/CardDeckTest.java
+++ src/test/java/no/trygveaa/blackjack/CardDeckTest.java
@@ -2,6 +2,10 @@ package no.trygveaa.blackjack;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CardDeckTest {
@@ -12,5 +16,36 @@ class CardDeckTest {
         int numberOfCards = CardSuit.values().length * CardRank.values().length;
 
         assertThat(deck.getCards().size()).isEqualTo(numberOfCards);
+    }
+
+    @Test
+    void shouldReturnFirstCardOnDraw() {
+        Card card1 = new Card(CardSuit.CLUBS, CardRank.ACE);
+        Card card2 = new Card(CardSuit.HEARTS, CardRank.RANK2);
+        CardDeck deck = new CardDeck(Arrays.asList(card1, card2));
+
+        Optional<Card> drawn = deck.draw();
+
+        assertThat(drawn).contains(card1);
+    }
+
+    @Test
+    void shouldRemoveCardOnDraw() {
+        Card card1 = new Card(CardSuit.CLUBS, CardRank.ACE);
+        Card card2 = new Card(CardSuit.HEARTS, CardRank.RANK2);
+        CardDeck deck = new CardDeck(Arrays.asList(card1, card2));
+
+        deck.draw();
+
+        assertThat(deck.getCards().size()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReturnEmptyWhenDeckIsEmpty() {
+        CardDeck deck = new CardDeck(Collections.emptyList());
+
+        Optional<Card> drawn = deck.draw();
+
+        assertThat(drawn).isEmpty();
     }
 }
